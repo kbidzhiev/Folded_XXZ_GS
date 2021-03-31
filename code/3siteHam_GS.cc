36d652f07a7341236f493fe0ae49ab8ecff46aa4
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-03-31 13:14:20 +0200

    Nomenclature is changed. j = 0 is impurity site. The UP spins are 2,4,6 (even)

--- code/3siteHam_GS.cc
+++ code/3siteHam_GS.cc
@@ -131,7 +131,7 @@ public:
 		operator[]("hL") = 0; //chemical potential
 		operator[]("hR") = 0;
 		operator[]("h") = 2.0;
-		operator[]("rho") = 0;
+		operator[]("rho") = 0.0;
 		operator[]("Q1Profile") = 0; // energy and current profile
 		operator[]("Q2Profile") = 0;
 		operator[]("Current") = 0;
@@ -171,7 +171,7 @@ private:
 		//if ((2*N)<=3) cout<<"Error, N="<<N<<" is too small.\n",exit(0);
 		for (int j = 1; j < N-1; ++j) {
 			//Strange coefficients are needed to match with
-			// spin Pauli matrices instead of Sx Sy
+			// Pauli matrices instead of spin Sx Sy
 			ampo += J * 4 * 0.25, "S+", j, "S-", j + 2; // 0.5 (SpSm+ SmSp) = SxSx + SySy
 			ampo += J * 4 * 0.25, "S-", j, "S+", j + 2;
 			ampo += J * -8 * 0.25, "S+", j, "Sz", j + 1, "S-", j + 2;
@@ -210,42 +210,44 @@ private:
 	string ham_type;
 	void init(const ThreeSiteParam &param) {    //.init (param)
 		const double J = param.val("J");
-		//const double h = param.val("h");
 		auto h = [&](double j){
-			return 2.0*cos(j);
+			return param.val("h");
+			//return 2.0*cos(j); // this term add "chaotic" h
 		};
 		const double rho = param.val("rho");
 		double m = 1.0;
 
 
-		dot = N / 2;  //Position of the central spin
+		dot = (N + 1)/ 2;  //Position of the central spin
 
 		if (ham_type == "Ladder") {
-			for (int j = 1; j <= N - 3; j += 2) {
+			for (int j = 1; j <= N - 2; j += 2) {
 				// The coefficients are needed to match with
 				// spin Pauli matrices instead of Sx Sy
-				ampo += -J * m * 2, "Sz", j;
+				ampo += -J * m * 2, "Sz", j + 1; //Sublattice A even sites
 
-				ampo += -J * 4, "Sx", j + 1, "Sx", j + 3;
-				ampo += -J * (h(j+1) + pow(-1, (j + 1) / 2) * rho) * 2, "Sz", j + 1;
+				ampo += -J * 4, "Sz", j , "Sz", j + 2; //Sublattice B
+				ampo += -J * (h(j) + pow(-1, j / 2) * rho) * 2, "Sx", j; //Sublattice B
 
-				cout << "Sz : " << j << "\t SxSx : (" << j + 1 << ", " << j + 3
-						<< ")" << "\t h term ("
-						<< (h(j+1) + pow(-1, (j + 1) / 2) * rho) << "): " << j + 1
-						<< endl;
+//				cout << "Sz : " << j << "\t SxSx : (" << j + 1 << ", " << j + 3
+//						<< ")" << "\t h term ("
+//						<< (h(j+1) + pow(-1, (j + 1) / 2) * rho) << "): " << j + 1
+//						<< endl;
 			}
 			cout << "Sz : " << N - 1 << "\t \t \t \t h term ("
 					<< (h(N) + pow(-1, N / 2) * rho) << "): " << N << endl;
-			ampo += -J * m * 2, "Sz", N - 1;
-			ampo += -J * (h(N) + pow(-1, N / 2) * rho) * 2, "Sz", N;
+			ampo += -J * m * 2, "Sz", N - 1; // Sublattice A even sites
+
+			ampo += -J * (h(N) + pow(-1, N / 2) * rho) * 2, "Sx", N;
 		} else if (ham_type == "Ising"){
+			// to create an initial state as GS of ladder ham, we start with GS
+			// of uniform ising model to introduce initial correlations,
+			// otherwise DMRG procedure cannot find a GS
 			for (int j = 1; j < N; j++){
-				ampo += -J * 4, "Sx", j, "Sx", j+1;
-				ampo += -J * (h(j) + pow(-1, j) * rho) * 2, "Sz", j;
-				//ampo += -J * (m + 0.5) * 2, "Sz", j;
+				ampo += -J * 4, "Sz", j, "Sz", j+1;
+				ampo += -J * m * 2, "Sz", j;
 			}
-			ampo += -J * (h(N) + pow(-1, N) * rho) * 2, "Sz", N;
-			//ampo += -J * (m + 0.5) * 2, "Sz", N;
+			ampo += -J * m * 2, "Sz", N;
 
 		} else {
 			throw invalid_argument("One should choose Ladder or Ising in the LadderHamiltonian initialization");
@@ -521,10 +523,10 @@ int main(int argc, char *argv[]) {
 
 		cout << "Norm (before unitary gates )is = " << real(innerC(psi, psi)) << endl;
 
-		HadamarGate(N/2 - 1);
-		HadamarGate(N/2    );
-		HadamarGate(N/2 + 1);
-		HadamarGate(N/2 + 2);
+//		HadamarGate(N/2 - 1);
+//		HadamarGate(N/2    );
+//		HadamarGate(N/2 + 1);
+//		HadamarGate(N/2 + 2);
 
 
 		psi.noPrime();
