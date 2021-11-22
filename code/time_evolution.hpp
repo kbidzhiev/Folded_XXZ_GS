4dfe63d84b717c19dd05be08bcf6bf4b09a2dbd1
kbidzhiev <bidzhiev.kemal@physics.msu.ru>
2021-11-22 20:53:20 +0100

    PPK

--- code/time_evolution.hpp
+++ code/time_evolution.hpp
@@ -142,6 +142,25 @@ private:
 
 
 
+//Trotter Gates for the time evolution of Folded_XXZ + (1-Z)(1-Z)(SpSm+SmSp)
+class TrotterExp_PPK {
+public:
+	struct TGate {
+		int i1 = 0;
+		ITensor G;
+		TGate(int i1_, ITensor G_)
+		: i1(i1_)
+		, G(G_) {
+		}
+	};
+	TrotterExp(const SiteSet &sites, const ThreeSiteParam &param, const complex<double> tau);
+	void initialize(const SiteSet &sites, const ThreeSiteParam &param, const complex<double> tau);
+	void TimeGates(const int begin, const int end, const complex<double> tau,
+			const SiteSet &sites, const ThreeSiteParam &param);
+	void Evolve(MPS &psi, const Args &args) ;
+private:
+	vector<TGate> gates;
+};
 
 
 
